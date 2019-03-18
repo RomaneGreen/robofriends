6b4c419ccacc7a59db0693785905c8a098d337d3
Romane G <romane71193@gmail.com>
2019-03-18 19:32:03 -0400

    add containers and components

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons'
-import App from './App'
-import './App.css'
+import App from './containers/App'
+
 
 ReactDOM.render( <div> <App /> </div>, document.getElementById('root'));
 
