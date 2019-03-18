235619d6b6df5e4a693cd9094b68131f7edc9444
Romane G <romane71193@gmail.com>
2019-03-18 19:05:47 -0400

    fetch users from json plceholder api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import CardList from './CardList';
-import { robots } from './robots';
 import SearchBox from './SearchBox'
 
 
@@ -9,9 +8,19 @@ class App extends Component {
     constructor() {
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
+    }
+
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response =>
+            response.json() )
+        .then(users => this.setState({ robots: users })
+        )
+        
     }
 
     onSearchChange = (event) => {
@@ -24,6 +33,9 @@ class App extends Component {
         const filteredRobots = this.state.robots.filter(robots => {
             return   robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
         })
+        if (this.state.robots.length === 0){
+            return <h1>Loading</h1>
+        }else {
   return (
     <div className= 'tc'>
         <h1 className='f1'>RoboFriends</h1>
@@ -32,6 +44,7 @@ class App extends Component {
     </div>
   )
     }
+}
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons'
-import robots from './robots'
 import App from './App'
-import CardList from './CardList'
 import './App.css'
 
 ReactDOM.render( <div> <App /> </div>, document.getElementById('root'));
