4f1d77fc78cef9d9c9c2dd1e67bb36c56021574e
Romane G <romane71193@gmail.com>
2019-03-25 01:58:02 -0400

    complete conversion to redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { createStore,applyMiddleware } from 'redux';
+import { createStore,applyMiddleware,combineReducers } from 'redux';
 import { Provider} from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons'
 import App from './containers/App'
-import { searchRobots } from './reducers';
+import { searchRobots, requestRobots } from './reducers';
 import { createLogger } from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
 
+const rootReducer = combineReducers({
+    searchRobots,
+    requestRobots
+})
 const logger = createLogger();
-const store = createStore(searchRobots, applyMiddleware(logger))
+const store = createStore(rootReducer, applyMiddleware(logger,thunkMiddleware))
 
 ReactDOM.render( <div><Provider store={store}> <App /> </Provider></div>, document.getElementById('root'));
 
